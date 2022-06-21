f44c10b7bd47e7c5f3c1148f918808bb5ce57883
Andres Agudelo <anagudelogu@gmail.com>
2022-06-21 06:51:22 -0500

    Fix linters

--- src/Services/airPollutionAPI.js
+++ src/Services/airPollutionAPI.js
@@ -4,8 +4,7 @@ const API_KEY = `&appid=${process.env.REACT_APP_AIR_POLLUTION_API_KEY}`;
 const URL = 'http://api.openweathermap.org/data/2.5/';
 const AIR_POLLUTION_ENDPOINT = 'air_pollution?';
 
-const finalUrl = (lat, lon) =>
-  `${URL + AIR_POLLUTION_ENDPOINT}lat=${lat}&lon=${lon}${API_KEY}`;
+const finalUrl = (lat, lon) => `${URL + AIR_POLLUTION_ENDPOINT}lat=${lat}&lon=${lon}${API_KEY}`;
 
 const getAirPollutionDataForCountry = async (lat, lon) => {
   const fetchRequest = new FetchRequest({ url: finalUrl(lat, lon) });
