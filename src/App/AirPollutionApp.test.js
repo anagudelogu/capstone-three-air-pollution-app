1a8aaaeb5ddaebbfd4bab87a61d096a4c5a9aa65
Andres Agudelo <anagudelogu@gmail.com>
2022-06-21 19:15:28 -0500

    Implement React router and test navigation

---
+++ src/App/AirPollutionApp.test.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import { Routes, Route, useParams } from 'react-router-dom';
+import { render, screen } from '../Mocks/test-utils';
+
+import AirPollutionApp from './AirPollutionApp';
+
+const DummyComp = () => {
+  const { regionName } = useParams();
+  return (
+    <h3>
+      {regionName}
+      {' '}
+      is a continent
+    </h3>
+  );
+};
+
+test('should navigate through pages', async () => {
+  const user = userEvent.setup();
+  render(
+    <Routes>
+      <Route path="/" element={<AirPollutionApp />}>
+        <Route path="/:regionName" element={<DummyComp />} />
+      </Route>
+    </Routes>,
+  );
+
+  let content = screen.queryByRole('heading', {
+    name: /america is a continent/i,
+  });
+
+  expect(content).toBeNull();
+
+  const navbarRegionsButton = screen.getByRole('button', {
+    name: /regions/i,
+  });
+
+  await user.click(navbarRegionsButton);
+
+  const americaLink = screen.getByText(/america/i);
+  const asiaLink = screen.getByText(/asia/i);
+  const africaLink = screen.getByText(/africa/i);
+  const europeLink = screen.getByText(/europe/i);
+  const oceaniaLink = screen.getByText(/oceania/i);
+
+  await user.click(americaLink);
+  content = screen.queryByRole('heading', {
+    name: /america is a continent/i,
+  });
+  expect(content).toBeInTheDocument();
+
+  await user.click(asiaLink);
+  content = screen.queryByRole('heading', {
+    name: /asia is a continent/i,
+  });
+  expect(content).toBeInTheDocument();
+
+  await user.click(africaLink);
+  content = screen.queryByRole('heading', {
+    name: /africa is a continent/i,
+  });
+  expect(content).toBeInTheDocument();
+
+  await user.click(europeLink);
+  content = screen.queryByRole('heading', {
+    name: /europe is a continent/i,
+  });
+  expect(content).toBeInTheDocument();
+
+  await user.click(oceaniaLink);
+  content = screen.queryByRole('heading', {
+    name: /oceania is a continent/i,
+  });
+  expect(content).toBeInTheDocument();
+});
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import AirPollutionApp from './App/AirPollutionApp';
 import './index.css';
 import worker from './Mocks/browser';
 import store from './App/store';
+import Home from './Containers/Home/Home';
 
 if (process.env.REACT_APP_STAGE === 'development') {
   worker.start();
@@ -14,7 +16,15 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <AirPollutionApp />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AirPollutionApp />}>
+            <Route index element={<Home />} />
+            <Route path=":regionName" element={<h2>Hello</h2>} />
+          </Route>
+          <Route path="*" element={<div>Not found</div>} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 );
